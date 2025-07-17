5a242ce9f3e5bc07b23eafa2f4bd134d8c4c1e40
Mohit Yadav Palam <mohitpalam09@gmail.com>
2025-07-17 18:22:29 -0500

    Got the statement class to work well

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Create some sample movies
         Movie movie1 = new Movie("The Matrix", Movie.NEW_RELEASE);
-        Movie movie2 = new Movie("Toy Story", Movie.CHILDRENS);
-        Movie movie3 = new Movie("John Wick", Movie.REGULAR);
+        Movie movie2 = new Movie("Frozen", Movie.CHILDRENS);
+        Rental rental1 = new Rental(movie1, 3);
+        Rental rental2 = new Rental(movie2, 5);
 
-        // Create rentals for those movies
-        Rental rental1 = new Rental(movie1, 3); // 3 days
-        Rental rental2 = new Rental(movie2, 5); // 5 days
-        Rental rental3 = new Rental(movie3, 2); // 2 days
-
-        // Create a customer and add the rentals
-        Customer customer = new Customer("John Doe");
+        Customer customer = new Customer("Alice");
         customer.addRental(rental1);
         customer.addRental(rental2);
-        customer.addRental(rental3);
 
-        // Print the plain text and XML statements
-        System.out.println(customer.prinStatement());
-        System.out.println();
-        System.out.println(customer.printStatementXML());
+        Statement statement = new Statement(customer);
+        System.out.println(statement.printStatement());
     }
 }
---
+++ Statement.java
@@ -0,0 +1,45 @@
+public class Statement {
+
+    private final Customer customer;
+
+    public Statement(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String printStatement() {
+        int frequentRenterPoints = 0;
+        double totalAmount = 0;
+        StringBuilder result = new StringBuilder("Rental Record for ");
+        result.append(customer.getName()).append("\n");
+
+        for (Rental rental : customer.getRentals()) {
+            double currentAmount = calculateRentalAmount(rental);
+            frequentRenterPoints += calculateFrequentRenterPoints(rental);
+
+            result.append(buildRentalLine(rental, currentAmount));
+            totalAmount += currentAmount;
+        }
+
+        result.append(buildFooter(totalAmount, frequentRenterPoints));
+        return result.toString();
+    }
+
+    private double calculateRentalAmount(Rental rental) {
+        return rental.getRentalAmount();
+    }
+
+    private int calculateFrequentRenterPoints(Rental rental) {
+        int points = 1;
+        points += rental.calculateFrequentRenterPoints();
+        return points;
+    }
+
+    private String buildRentalLine(Rental rental, double amount) {
+        return "\t" + rental.getMovie().getTitle() + "\t" + amount + "\n";
+    }
+
+    private String buildFooter(double totalAmount, int points) {
+        return "Amount owed is " + totalAmount + "\n" +
+               "You earned " + points + " frequent renter points";
+    }
+}
