46d7293c13f3549035a92ac52bbb1a7f427d77c5
Mohit Yadav Palam <mohitpalam09@gmail.com>
2025-08-12 15:29:56 -0500

    bulkrentalcoupon, freerentalcoupon,halfoffcoupon

--- BulkRentalCoupon.java
+++ BulkRentalCoupon.java
@@ -1,14 +1,16 @@
 public class BulkRentalCoupon implements DiscountPolicy {
     private final Customer customer;
+    private boolean discountUsed = false; // track if discount already applied
 
     public BulkRentalCoupon(Customer customer) {
         this.customer = customer;
     }
 
     @Override
-    public double applyDiscount(Rental rental, double charge) {
+    public double applyDiscount(Transaction transaction, double charge) {
         // Only apply $5 off to ONE rental if the customer has 5 or more rentals
-        if (customer.getRentals().size() >= 5) {
+        if (!discountUsed && customer.getTransactions().size() >= 5) {
+            discountUsed = true; // mark as used so it won’t apply again
             return Math.max(charge - 5.0, 0.0); // Avoid negative charge
         }
         return charge;
