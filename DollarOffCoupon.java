7ad4a40ea2f57c175133f543e429bbd99c7c1b38
Carlos A. Esquivel <esquivel.carlos09@gmail.com>
2025-08-12 15:30:47 -0500

    dollarOffCuponBugs

--- DollarOffCoupon.java
+++ DollarOffCoupon.java
@@ -1,9 +1,9 @@
 public class DollarOffCoupon implements DiscountPolicy {
     @Override
-    public double applyDiscount(Rental rental, double charge) {
-        if (charge > 5.0) {
-            return charge - 1.0;
+    public double applyDiscount(Transaction transaction, double originalPrice) {
+        if (originalPrice > 5.0) {
+            return originalPrice - 1.0;
         }
-        return charge;
+        return originalPrice;
     }
 }
